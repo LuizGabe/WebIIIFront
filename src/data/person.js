581e7b0256ea346f46940744b81f131056af7b62
Luiz Gabriel <luizgabrielgrupe@gmail.com>
2023-05-06 01:25:39 -0300

    initial commit

---
+++ src/data/person.js
@@ -0,0 +1,18 @@
+
+const Person = {
+  name: 'Luiz Gabriel',
+  office:"Junior Developer",
+  file: {
+    cover:"https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=50",
+    avatar:"https://github.com/luizgabe.png"
+  },
+  stacks: [
+    { Tec: "JavaScript", color: "#000" },
+    { Tec: "HTML", color: "red"},
+    { Tec: "CSS", color: "green" },
+    { Tec: "Typescript", color: "blue" },
+    { Tec: "React", color: "yellow" }
+  ]
+}
+
+export default Person
