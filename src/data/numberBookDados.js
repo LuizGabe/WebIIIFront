7d0ccab3afc1533127ebbc61d0387596ecc1a2a9
Luiz Gabriel <luizgabrielgrupe@gmail.com>
2023-05-08 02:20:49 -0300

    Add contacts and letters

---
+++ src/data/numberBookDados.js
@@ -0,0 +1,62 @@
+
+export const contatos = [
+  {
+    name: 'Brenda Mendes',
+    number: '(21) 90876-8765',
+    imgUrl: 'https://randomuser.me/api/portraits/men/14.jpg'
+  },
+  {
+    name: 'Abraão Sena',
+    number: '(11) 90876-1234',
+    imgUrl: 'https://randomuser.me/api/portraits/men/7.jpg'
+  },
+  {
+    name: 'Cleiton Souza',
+    number: '(11) 90876-1209',
+    imgUrl: 'https://randomuser.me/api/portraits/men/44.jpg'
+  },
+  {
+    name: 'Daniel Duarte',
+    number: '(82) 90876-5534',
+    imgUrl: 'https://randomuser.me/api/portraits/men/22.jpg'
+  },
+  {
+    name: 'Beatriz Clasen',
+    number: '(48) 90876-1123',
+    imgUrl: 'https://randomuser.me/api/portraits/men/33.jpg'
+  },
+  {
+    name: 'Caio Vinícius',
+    number: '(71) 90876-2435',
+    imgUrl: 'https://randomuser.me/api/portraits/men/27.jpg'
+  }
+]
+
+export const letras = [
+  { letter: 'a', color: '#1900FF', first: true },
+  { letter: 'b', color: '#1804F6', first: true },
+  { letter: 'c', color: '#1707EC', first: true },
+  { letter: 'd', color: '#160BE3', first: true },
+  { letter: 'e', color: '#150FDA', first: true },
+  { letter: 'f', color: '#1413D0', first: true },
+  { letter: 'g', color: '#1316C7', first: true },
+  { letter: 'h', color: '#121ABE', first: true },
+  { letter: 'i', color: '#111EB4', first: true },
+  { letter: 'j', color: '#1021AB', first: true },
+  { letter: 'k', color: '#0F25A2', first: true },
+  { letter: 'l', color: '#0E2998', first: true },
+  { letter: 'm', color: '#0D2D8F', first: true },
+  { letter: 'n', color: '#0C3086', first: true },
+  { letter: 'o', color: '#0B347D', first: true },
+  { letter: 'p', color: '#0A3873', first: true },
+  { letter: 'q', color: '#093C6A', first: true },
+  { letter: 'r', color: '#083F61', first: true },
+  { letter: 's', color: '#074357', first: true },
+  { letter: 't', color: '#06474E', first: true },
+  { letter: 'u', color: '#054A45', first: true },
+  { letter: 'v', color: '#044E3B', first: true },
+  { letter: 'w', color: '#035232', first: true },
+  { letter: 'x', color: '#025629', first: true },
+  { letter: 'y', color: '#01591F', first: true },
+  { letter: 'z', color: '#005D16', first: true }
+]
